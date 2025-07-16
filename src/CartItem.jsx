4bb23ea498f7c7731eb9df3d0c30557c8028d3b7
mathu2112 <mathushawlika1221@gmail.com>
2025-07-16 23:06:08 +0400

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux'; // <-- DUPLICATE
+
 
 const CartItem = ({ onContinueShopping }) => {
+  const [showCart, setShowCart] = useState(true);
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
@@ -12,7 +14,7 @@ const CartItem = ({ onContinueShopping }) => {
   const calculateTotalAmount = () => {
     let total=0;
     cart.forEach(item=>{
-        quantity=item.quantity;
+        const quantity=item.quantity;
         const cost=parseFloat(item.cost.substring(1));
         total+=cost*quantity
     });
@@ -25,6 +27,14 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
     
   };
+  const handleAddToCart = (product) => {
+  dispatch(addItem({
+    name: product.name,
+    cost: product.cost,
+    image: product.image,
+    quantity: 1
+  }));
+};
 
 
   const handleIncrement = (item) => {
@@ -40,7 +50,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem(itemName))
+    dispatch(removeItem(item.name))
   };
   const handleCheckoutShopping = (e) => {
   alert('Functionality to be added for future reference');
